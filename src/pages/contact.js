178b918c2da71f876a9ffb91cd0eba5372c43d10
Joshua Sulkers <josh.sulkers@gmail.com>
2021-01-23 15:50:43 -0800

    Add rest of Gatsby tutorial
    Ready to test via S3 or Amplify or something.

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+
+export default function Contact() {
+  return (
+    <div>
+      Contact
+      <Link to="/">Home</Link>
+    </div>
+  )
+}
