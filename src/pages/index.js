5ece55e1a3b4ce7f03eb01aee66e1fbf5e7c2fa5
Joshua Sulkers <josh.sulkers@gmail.com>
2021-01-23 21:40:13 -0800

    Just a manual test

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
       <h1>Some styling</h1>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
       <Link to="/contact/">Contact</Link>
+      <h1>Test</h1>
     </div>
   )
 }
