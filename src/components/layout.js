3e81d9190a8392ff0192bb8e0bf62a5444cda3d2
Joshua Sulkers <josh.sulkers@gmail.com>
2021-01-30 14:57:53 -0800

    Add picture and some CSS around it
    This was just to play around, plan now is to go back and clean up.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
+import SulkersImg from "/src/images/sulkers.jpg"
+import Container from "../components/container"
+
+
 const ListLink = props => (
   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
     <Link to={props.to}>{props.children}</Link>
@@ -11,7 +15,7 @@ export default function Layout({children}) {
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
       <header style={{ marginBottom: `1.5rem` }}>
         <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-          <h3 style={{ display: `inline` }}>MySweetSite</h3>
+          <h3 style={{ display: `inline` }}>Josh Sulkers</h3>
         </Link>
         <ul style={{ listStyle: `none`, float: `right` }}>
           <ListLink to="/">Home</ListLink>
@@ -19,7 +23,10 @@ export default function Layout({children}) {
           <ListLink to="/contact/">Contact</ListLink>
         </ul>
       </header>
+      <Container>
+      <img src={SulkersImg} id="bg" alt="Pic of Josh" />
       {children}
+      </Container>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,8 @@ import Layout from "../components/layout"
 export default function Home() {
   return (
     <Layout>
-      <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
-        <h1>Some styling</h1>
-        <img src="https://source.unsplash.com/random/400x200" alt="" />
+      <div>
+        <h1>Josh Sulkers</h1>
         <Link to="/contact/">Contact</Link>
         <h1>Test</h1>
       </div>
